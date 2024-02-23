196e5f095d890c33d452eda3faf4143b2cddd90f
Viviana Rosenbaum <vivianarosenbaum@gmail.com>
2024-02-22 20:41:28 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/users", (req, res) => {
+  let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+  res.status(200).send(friends);
+});
+
+app.get("/weather/:temperature", (req, res) => {
+  const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+  res.status(200).send(phrase);
+});
+
+app.listen(4000, () => console.log("Server running on 4000"));
